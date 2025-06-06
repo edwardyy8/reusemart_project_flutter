c029ebc9050fc3fa882a193ce42e47858a309b30
lyjessn <jslynazr@gmail.com>
2025-06-06 16:10:25 +0700

    ganti model barang

--- lib/entity/barang.dart
+++ lib/entity/barang.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+ 
 class Barang {
   String? idBarang;
   String? idPenitip;
@@ -7,14 +7,14 @@ class Barang {
   String? namaBarang;
   int? stokBarang;
   double? hargaBarang;
-  String? garansiBarang;
+  String? garansi;
   String? statusBarang;
   String? deskripsiBarang;
   DateTime? tanggalMasuk;
   double? beratBarang;
   DateTime? tanggalGaransi;
   String? fotoBarang;
-
+ 
   Barang({
     this.idBarang,
     this.idPenitip,
@@ -22,7 +22,7 @@ class Barang {
     this.namaBarang,
     this.stokBarang,
     this.hargaBarang,
-    this.garansiBarang,
+    this.garansi,
     this.statusBarang,
     this.deskripsiBarang,
     this.tanggalMasuk,
@@ -30,26 +30,30 @@ class Barang {
     this.tanggalGaransi,
     this.fotoBarang,
   });
-
+ 
   factory Barang.fromRawJson(String str) => Barang.fromJson(json.decode(str));
   factory Barang.fromJson(Map<String, dynamic> json) {
     return Barang(
-      idBarang: json["id_barang"],
-      idPenitip: json["id_penitip"],
-      idKategori: json["id_kategori"],
-      namaBarang: json["nama_barang"],
-      stokBarang: json["stok_barang"],
-      hargaBarang: json["harga_barang"].toDouble(),
-      garansiBarang: json["garansi"],
-      statusBarang: json["status_barang"],
-      deskripsiBarang: json["deskripsi"],
-      tanggalMasuk: DateTime.parse(json["tanggal_masuk"]),
-      beratBarang: json["berat_barang"].toDouble(),
-      tanggalGaransi: DateTime.parse(json["tanggal_garansi"]),
-      fotoBarang: json["foto_barang"],
+      idBarang: json["id_barang"] as String?,
+      idPenitip: json["id_penitip"] as String?,
+      idKategori: (json['id_kategori'] as num?)?.toInt(),
+      namaBarang: json["nama_barang"] as String?,
+      stokBarang: (json['stok_barang'] as num?)?.toInt(),
+      hargaBarang: (json["harga_barang"] as num?)?.toDouble(),
+      garansi: json["garansi"] as String?,
+      statusBarang: json["status_barang"] as String?,
+      deskripsiBarang: json["deskripsi"] as String?,
+      tanggalMasuk: json['tanggal_masuk'] != null
+          ? DateTime.tryParse(json['tanggal_masuk'] as String)
+          : null,
+      beratBarang: (json["berat_barang"] as num?)?.toDouble(),
+      tanggalGaransi: json['tanggal_garansi'] != null
+          ? DateTime.tryParse(json['tanggal_garansi'] as String)
+          : null,
+      fotoBarang: json["foto_barang"] as String?,
     );
   }
-
+ 
   Map<String, dynamic> toJson() {
     return {
       "id_barang": idBarang,
@@ -58,7 +62,7 @@ class Barang {
       "nama_barang": namaBarang,
       "stok_barang": stokBarang,
       "harga_barang": hargaBarang,
-      "garansi": garansiBarang,
+      "garansi": garansi,
       "status_barang": statusBarang,
       "deskripsi": deskripsiBarang,
       "tanggal_masuk": tanggalMasuk?.toIso8601String(),
